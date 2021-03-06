1c5763808ee0c0ad90e92f992d7a6ef72d974470
svmcd <spolat0750@gmail.com>
2021-03-06 20:23:13 +0100

    sidenav responsive gemaakt & kleine fixes & begonnen aan lightmode switch

--- script.js
+++ script.js
@@ -7,3 +7,39 @@ function redirect(){
 }
 
 document.getElementById('welkom').innerHTML = "Welkom, " + (localStorage.getItem("storage"));
+
+const menuIconEl = $('.menu-icon');
+const sidenavEl = $('.side-nav');
+const sidenavCloseEl = $('.sidenav-close-icon');
+
+function toggleClassName(el, className) {
+  if (el.hasClass(className)) {
+    el.removeClass(className);
+  } else {
+    el.addClass(className);
+  }
+}
+
+menuIconEl.on('click', function() {
+  toggleClassName(sidenavEl, 'active');
+});
+
+sidenavCloseEl.on('click', function() {
+  toggleClassName(sidenavEl, 'active');
+});
+
+$('.btn-background').on('click', function () {
+    $('.header').toggleClass('color');
+    $('.main').toggleClass('color');
+    $('.side-nav').toggleClass('color');
+    $('.widget').toggleClass('color');
+    $('.footer').toggleClass('color');
+    $('.zoeken').toggleClass('color');
+    $('.btn-face').toggleClass('move');
+    
+
+
+
+
+
+})
