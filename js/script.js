0e32f34cc0aab21d82fee8f828db34168dc0c4cc
svmcd <spolat0750@gmail.com>
2021-03-22 12:41:26 +0100

    muziekspeler (nog niet af)

--- js/script.js
+++ js/script.js
@@ -57,10 +57,51 @@ $('.btn-background').on('click', function () {
     $('.btn-background').toggleClass('color');
     $('.btn-face').toggleClass('move');
     $('.fa-github').toggleClass('color');
-    
-
-
-
-
-
+  
 })
+
+music_name = "../files/sky.mp3"
+let play_btn = document.querySelector("#play");
+let prev_btn = document.querySelector("#prev");
+let next_btn = document.querySelector("#next");
+let range = document.querySelector("#range");
+let total_time = 0;
+let currentTime = 0;
+let isPlaying = false;
+let song = new Audio();
+window.onload = playSong;
+
+function playSong(){
+    song.src = music_name;
+    console.log(song)
+    
+    
+    play_btn.addEventListener('click',function(){
+        if(!isPlaying){
+            song.play();
+            isPlaying = true;
+            total_time = song.duration;
+            range.max = total_time;
+            $(play_btn).removeClass("fas fa-play")            
+            $(play_btn).toggleClass("fas fa-pause")
+          }else{
+            song.pause();
+            isPlaying = false;
+            $(play_btn).toggleClass("fas fa-pause")
+        }
+       song.addEventListener('ended',function(){
+            song.currentTime = 0
+            song.pause();
+            isPlaying = false;
+            range.value = 0;
+            $(play_btn).toggleClass("fas fa-play")
+        })
+        song.addEventListener('timeupdate',function(){
+            range.value = song.currentTime;
+        })
+        range.addEventListener('change',function(){
+            song.currentTime = range.value;
+        })
+       
+    })
+}
