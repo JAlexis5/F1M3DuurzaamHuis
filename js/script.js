7ee79ab33837e8ed5b264d897b9415d6ffada9f1
svmcd <spolat0750@gmail.com>
2021-03-27 14:51:07 +0100

    big update

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ $('.random').each(function () {
   $(this).text(x)
 });
 
-document.getElementById('welkom').innerHTML = " " + (localStorage.getItem("storage"));
+document.getElementById('welkom').innerHTML = "Hoi " + (localStorage.getItem("storage")) + ", ";
 
 const menuIconEl = $('.menu-icon');
 const sidenavEl = $('.side-nav');
@@ -34,10 +34,26 @@ sidenavCloseEl.on('click', function() {
   toggleClassName(sidenavEl, 'active');
 });
 
+let image1 = document.getElementById("circle-image1")
+let image2 = document.getElementById("circle-image2")
+let image3 = document.getElementById("circle-image3")
+let image4 = document.getElementById("circle-image4")
+
 $('.btn-background').on('click', function () {
     $('.body').toggleClass('color');
     $('.header').toggleClass('color');
     $('.main').toggleClass('color');
+    $('#circle-image1').toggleClass('color');
+    $('#circle-image1-b').toggleClass('color');
+    $('#circle-image2').toggleClass('color');
+    $('#circle-image2-b').toggleClass('color');
+    $('#circle-image3').toggleClass('color');
+    $('#circle-image3-b').toggleClass('color');
+    $('#circle-image4').toggleClass('color');
+    $('#circle-image4-b').toggleClass('color');
+    $('.batterij-tekst').toggleClass('color');
+    $('.lightmode').toggleClass('color');
+    $('.darkmode').toggleClass('color');
     $('.side-nav').toggleClass('color');
     $('.widget').toggleClass('color');
     $('.footer').toggleClass('color');
@@ -56,15 +72,19 @@ $('.btn-background').on('click', function () {
     $('.btn-face').toggleClass('color');
     $('.btn-background').toggleClass('color');
     $('.btn-face').toggleClass('move');
+    $('.widget7-tekst1').toggleClass('color');
+    $('.widget7-tekst2').toggleClass('color');
+    $('.cover-dark').toggleClass('color');
+    $('.cover-light').toggleClass('color');
     $('.fa-github').toggleClass('color');
-  
 })
 
-music_name = "../files/sky.mp3"
+music_name = "http://127.0.0.1:5500/files/sky.mp3"
 let play_btn = document.querySelector("#play");
 let prev_btn = document.querySelector("#prev");
 let next_btn = document.querySelector("#next");
 let range = document.querySelector("#range");
+let play_img = document.querySelector("#play_img")
 let total_time = 0;
 let currentTime = 0;
 let isPlaying = false;
@@ -82,19 +102,21 @@ function playSong(){
             isPlaying = true;
             total_time = song.duration;
             range.max = total_time;
-            $(play_btn).removeClass("fas fa-play")            
-            $(play_btn).toggleClass("fas fa-pause")
+            play_img.src = "http://127.0.0.1:5500/images/pause.svg";
+          $('#muziek-cover').toggleClass('muziek-animation');
           }else{
             song.pause();
             isPlaying = false;
-            $(play_btn).toggleClass("fas fa-pause")
+            play_img.src = "http://127.0.0.1:5500/images/play.svg";
+          $('#muziek-cover').removeClass('muziek-animation');
         }
        song.addEventListener('ended',function(){
             song.currentTime = 0
             song.pause();
             isPlaying = false;
             range.value = 0;
-            $(play_btn).toggleClass("fas fa-play")
+            play_img.src = "http://127.0.0.1:5500/images/play.svg";
+          $('#muziek-cover').removeClass('muziek-animation');
         })
         song.addEventListener('timeupdate',function(){
             range.value = song.currentTime;
@@ -104,4 +126,55 @@ function playSong(){
         })
        
     })
+}
+
+$("input[type=range]").mousemove(function (e) {
+  var val = ($(this).val() - $(this).attr('min')) / ($(this).attr('max') - $(this).attr('min'));
+  var percent = val * 100;
+
+  $(this).css('background-image',
+      '-webkit-gradient(linear, left top, right top, ' +
+      'color-stop(' + percent + '%, #0b8e36), ' +
+      'color-stop(' + percent + '%, #101010)' +
+      ')');
+
+  $(this).css('background-image',
+      '-moz-linear-gradient(left center, #101010 0%, #0b8e36)');
+});
+
+var gasdata = new XMLHttpRequest();
+var url = "data-map/gasverbruik.json"
+gasdata.open("GET", url, true);
+gasdata.send();
+gasdata.onreadystatechange = function(){
+  if(this.readyState == 4 && this.status == 200){
+    var data = JSON.parse(this.responseText);
+    var months = data.maand_verbruik.map(function(elem) {
+      return elem.datum; 
+    });
+    var verbruik = data.maand_verbruik.map(function(elem) {
+      return elem.verbruik; 
+    });
+    var ctx = document.getElementById('canvas').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: months,
+            datasets: [{
+                label: 'Gasverbruik (m3) per maand',
+                data: verbruik, 
+                backgroundColor: ['#3aaa35', '#0b8e36', '#076633', '#3aaa35', '#0b8e36', '#076633', '#3aaa35', '#0b8e36', '#076633', '#3aaa35', '#0b8e36', '#076633',],
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+  }
 }
